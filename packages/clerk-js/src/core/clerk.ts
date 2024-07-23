14f5ae6f3248bac1ab90368711c6831892ac7959
Bryce Kalow <bryce@clerk.dev>
2024-07-23 11:45:29 -0500

    fix(clerk-js): Call session.touch() when a page is focused instead of only on visibility change

--- packages/clerk-js/src/core/clerk.ts
+++ packages/clerk-js/src/core/clerk.ts
@@ -1653,7 +1653,7 @@ export class Clerk implements ClerkInterface {
       return;
     }
 
-    this.#pageLifecycle?.onPageVisible(() => {
+    this.#pageLifecycle?.onPageFocus(() => {
       if (this.session) {
         void this.#touchLastActiveSession(this.session);
       }
